79e476dfeb75fd8a054dbea52417cdd852cdcfa5
sopian <sopian@eluon.com>
2024-12-01 15:08:06 +0700

    roolback excel download

--- controllers/pmkpController.js
+++ controllers/pmkpController.js
@@ -1373,9 +1373,12 @@ sikatApp.controller(
         unit: $rootScope.currPage
       };
 
+      /*
       const url = REPORT_URL + "/pdf_c/" + $scope.currPage + "/Rekap";
       pmkpService.postDownload(url, data, "Report Form C "+ $scope.formatString($scope.currPage) + ".pdf");
-
+      */
+      const url = REPORT_URL + "/xlsx/" + $scope.currPage;
+      pmkpService.postDownload(url, data, $scope.currPage + ".xlsx");
     };
 
     $scope.downloadChart = (part) => {
